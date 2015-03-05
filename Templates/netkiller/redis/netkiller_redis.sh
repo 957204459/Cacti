762301ee59bafde18e81f1f4abeb890840fdc746
netkiller <netkiller@msn.com>
2015-03-05 14:26:34 +0800

    redis

--- Templates/netkiller/redis/netkiller_redis.sh
+++ Templates/netkiller/redis/netkiller_redis.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 ipaddr=$1
+db=$2
+if [ -z $db ]; then
 #(echo -en "info\r\nquit\r\n"; sleep 0.5) | nc ${ipaddr} 6379 | tail -n +2 | head -n -2 | tr "\r\n" " " | grep connected_clients
-(echo -en "info\r\nquit\r\n"; sleep 0.5) | nc ${ipaddr} 6379 | egrep "(connected_clients|total_connections_received|total_commands_processed|keyspace_hits|keyspace_misses)" | tr -s '\r\n' ' ' | sed 's/[ ]*$/\n/g'
+(echo -en "info\r\nquit\r\n"; sleep 0.5) | nc ${ipaddr} 6379 | egrep "(connected_clients|total_connections_received|total_commands_processed|keyspace_hits|keyspace_misses|used_memory:)" | tr -s '\r\n' ' ' | sed 's/[ ]*$/\n/g'
+else
+	(echo -en "info\r\nquit\r\n"; sleep 0.5) | nc ${ipaddr} 6379 | grep "$db:" | cut -d ':' -f2 | sed -e "s/,/ /g" -e "s/=/:/g"
+fi
